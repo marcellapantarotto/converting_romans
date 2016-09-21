8fb6c33019a5be34ab738660e9bd29305feff663
marcellapantarotto <cella_pantarotto@hotmail.com>
2016-09-21 01:01:07 -0300

    Create tests.cpp
    Iniciação dos tests

---
+++ tests.cpp
@@ -0,0 +1,30 @@
+/*
+	CONVERTENDO NUMEORS ROMANOS
+
+	Aluna: Marcella Pantarotto
+	Matrícula: 13/0143880
+	Disciplina: Métodos de Programação - UnB
+	Professor: Jan Correa
+*/
+
+#include <stdio.h>
+#include <gtest/gtest.h>
+#include "conversão_algarismos.c"
+
+double roman (const double);
+
+TEST (RomanConversionTest, PositiveNos) { 
+    EXPECT_EQ (4, roman (IV));
+    EXPECT_EQ (5, roman (V));
+    EXPECT_EQ (6, roman (VII));
+    EXPECT_EQ (9, roman (IX));
+}
+ 
+TEST (RomanConversionTest, NegativeNos) { 
+
+}
+
+int main(int argc, char **argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
