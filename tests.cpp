228b198c66221e427cced814adcb964d24db5db9
marcellapantarotto <marcellapantarotto@gmail.com>
2016-09-21 02:18:14 -0300

    Update tests.cpp
    Adição de testes.

--- tests.cpp
+++ tests.cpp
@@ -9,22 +9,27 @@
 
 #include <stdio.h>
 #include <gtest/gtest.h>
-#include "roman.c"
+#include "conversão_algarismos.c"
 
 double roman (const double);
 
-TEST (RomanConversionTest, PositiveNos) { 
+TEST (RomanConversionTest, Positive) { 
+    EXPECT_EQ (3, roman (III));
     EXPECT_EQ (4, roman (IV));
     EXPECT_EQ (5, roman (V));
-    EXPECT_EQ (6, roman (VII));
-    EXPECT_EQ (9, roman (IX));
+    EXPECT_EQ (11, roman (XI));
+    EXPECT_EQ (22, roman (XXII));
+    EXPECT_EQ (40, roman (XL));
+    EXPECT_EQ (75, roman (LXXV));
 }
  
-TEST (RomanConversionTest, NegativeNos) { 
+/*
+TEST (RomanConversionTest, Negative) { 
 
 }
+*/
 
 int main(int argc, char **argv) {
-  ::testing::InitGoogleTest(&argc, argv);
+  ::testing::InitGoogleTest(&argc, argv);   //testar sem os primeiros ::
   return RUN_ALL_TESTS();
 }
