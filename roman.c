eeed56f41ec7bd03b3deff3b3015d7a3698f2bff
marcellapantarotto <mpantarotto@aluno.unb.br>
2016-09-22 19:44:29 -0300

    Update roman.c
    Implementação de condicionais nos cases de uso para seguir a lógica dos número romanos onde nenhum letra é repetida mais de 3 vezes e as condições de soma e subitração

--- roman.c
+++ roman.c
@@ -6,40 +6,125 @@
 	Disciplina: Métodos de Programação - UnB
 	Professor: Jan Correa
 */
-
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+//#include "roman.hpp"
+
 
 // funçao que analisa o numero romano inserido e faz as devidas conversoes para transforma-lo em numero decimal
-int digit(char c){
-	int value;
-	switch(c){
-		//uso do uppercase para nao haver importancia de letras minusculas ou maiusculas
-		uppercase 'I': value = 1; break;  // I pode ser usado no máximo 3 vezes em cada número
-		uppercase 'IV': value = 4; break; 
-		uppercase 'V': value = 5; break;
-		uppercase 'IX': value = 9; break; 
-		uppercase 'X': value = 10; break;
-		uppercase 'XL': value = 40; break; 
-		uppercase 'L': value = 50; break;
-		uppercase 'XC': value = 90; break; 
-		uppercase 'C': value = 100; break;
-		uppercase 'CD': value = 400; break; 
-		uppercase 'D': value = 500; break;
-		uppercase 'CM': value = 900; break; 
-		uppercase 'M': value = 1000; break;
-		uppercase '0': value = 0; break;
-		default: value = -1;
+int digit(char *c){
+	int value=0, i;
+	char aux;
+	int tam = (int)(strlen(c)); 
+
+	for(i=tam; i >= 0; i--){
+		aux = c[i];
+		switch(aux){
+
+			//numero 1
+			case 'i':
+			case 'I':{
+				if(value < 3){
+				}else{
+					if(value == 5 || value >= 10){
+					}else{
+						return -1;
+					}
+				}
+				break;
+			} 
+
+			//numero 5
+			case 'v':
+			case 'V':{
+				if(value < 5) value = value + 5;
+				break;
+			}
+
+			//numero 10
+			case 'x':
+			case 'X':{
+				if(value < 30){
+					printf(" 1X v=%d\n", value);
+				}else{
+					if(value >= 50 || value >= 100){
+						printf(" 2X v=%d\n", value);
+					}else{
+						return -1;
+					}
+				}
+				break;
+			}
+
+
+			//numero 50
+			case 'l':
+			case 'L':{
+				if(value < 60){
+					value =  value + 50;
+				}else{
+					if((c[i+1] == 'L' && value < 60) || (c[i+1] == 'C' && value < 110)){
+						value = value - 1;
+					}else{
+						return -1;
+					}
+				}
+				break;
+			}
+
+			//numero 100
+			case 'c':
+			case 'C':{
+				if(value < 300){
+					value = value + 100;
+				}else{
+					if(value >= 500 || value >= 1000){
+						printf(" 2C v=%d\n", value);
+					}else{
+						return -1;
+					}
+				}
+				break;
+			}
+
+			//numero 500
+			case 'd':
+			case 'D':{
+				if(value < 600){
+					value =  value + 500; 
+				}else{
+					if((c[i+1] == 'D' && value < 600) || (c[i+1] == 'M' && value < 1100)){
+						value = value - 100;
+					}else{
+						return -1;
+					}
+				}
+				break;
+			}
+
+			//numero 1000
+			case 'm':
+			case 'M':{
+				if(value < 3000){
+					value = value + 1000;
+				}
+				break;
+			}
+
+			//default: return -1;
+		}
 	}
 	return value;
+
 }
+
+
 
 
 int main(){
 	char roman[30];
-	long int number = 0;
-	int i = 0;
+	int number;
 
 	/*
 	if (roman<=0 || roman>3000){
@@ -48,30 +133,18 @@ int main(){
 	}
 	*/
 
-	printf("\n Escreva um numero romano (digitos validos: I, V, X, L, C, D, M): ");
+	printf("\n Escreva um numero romano ate 3000 (digitos validos: I, V, X, L, C, D, M): ");
 	scanf("%s", roman);
 	getchar();
 
-	if(digit(roman[i]) < 0){
-		printf("\n 1 Numero romano invalido: %c\n\n", roman[i]);
+	number = digit(roman);
+
+	if(number < 0){
+		printf("\n Numero romano invalido: %d\n\n", number);
 		return 0;
 	}
 
-
-	while(roman[i]!='\0' && ){
-		if(digit(roman[i])){
-			if (digit(roman[i]) >= digit(roman[i+1])){
-				number = number + digit(roman[i]);
-				i++;
-			}else{
-				number = number + (digit(roman[i+1]) - digit(roman[i]));
-				i++;
-			}
-			//i++;
-		}
-	}
-
-	printf("\n O numero na base decimal é igual a: %ld\n\n", number);
+	printf("\n O numero na base decimal é igual a: %d\n\n", number);
 
 	return 0;
 }
