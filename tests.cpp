fe7b3c80660be6784bc5f26d2454180cb5396b5c
marcellapantarotto <mpantarotto@aluno.unb.br>
2016-09-23 00:31:40 -0300

    Update tests.cpp

--- tests.cpp
+++ tests.cpp
@@ -1,11 +1,3 @@
-/*
-	CONVERTENDO NUMEROS ROMANOS
-
-	Aluna: Marcella Pantarotto
-	Matrícula: 13/0143880
-	Disciplina: Métodos de Programação - UnB
-	Professor: Jan Correa
-*/
 #include <iostream>
 #include <gtest/gtest.h>
 #include "roman.cpp"
@@ -29,10 +21,10 @@ TEST (RomanConversionTest, Negative) { //tentativa:
 	EXPECT_EQ (-1, roman (IIII));		//4
 	EXPECT_EQ (-1, roman (XCV));		//95
 	EXPECT_EQ (-1, roman (XCCV));		//195
-	EXPECT_EQ (-1, roman (LL));			//100
+	EXPECT_EQ (-1, roman (LL));		//100
 	EXPECT_EQ (-1, roman (IIX));		//7
 	EXPECT_EQ (-1, roman (VVI));		//11
-	EXPECT_EQ (-1, roman (XD));			//490
+	EXPECT_EQ (-1, roman (XD));		//490
 }
 
 
